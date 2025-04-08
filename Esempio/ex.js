4e7c3a4acd139a562cce675248ef2aa00b5035f7
Lorenzoo2208 <baiginilorenzo22@gmail.com>
2025-04-08 09:39:13 +0200

    base

---
+++ Esempio/ex.js
@@ -0,0 +1,13 @@
+// Base structure for a JavaScript file
+
+// Import necessary modules (if any)
+
+// Define constants and variables
+
+// Define functions
+function main() {
+    console.log("Hello, World!");
+}
+
+// Execute main function
+main();
